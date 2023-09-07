22415892ee28e62102fd1a14929fd16537a96e57
Frederick Morales pc1 <tocomorales30@gmail.com>
2023-09-07 14:18:18 -0600

    Terminando los estilos de los 3 temas de la calculadora

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 let resultContainer = document.getElementById('result')
-const numsContainer = document.getElementById('numsContainer')
+let numsContainer = document.getElementById('numsContainer')
 
 /*result es un string*/ 
 let result = resultContainer.innerHTML
@@ -7,6 +7,7 @@ let result = resultContainer.innerHTML
 numsContainer.addEventListener("click", returnValue)
 
 function returnValue(event){
+
     if(event.target.tagName === "DIV"){
         const parrafo = event.target.querySelector("p");
         if(parrafo){
@@ -14,23 +15,15 @@ function returnValue(event){
 
             if(valor === "RESET"){
                 result = " "
-                resultContainer.textContent  = result
+                resultContainer.textContent = result
             }else if(valor === "x"){
-                console.log(valor)
-                result = result + "*"
-                resultContainer.textContent  = result
+                result += "*"
+                resultContainer.textContent = result
             }else if(valor === "="){
                 showResult(result)
             }else if(valor === "DEL"){
-                if(typeof(result) === "string"){
-                    result = result.slice(0, -1)
-                    resultContainer.textContent  = result
-                }else{
-                    result = result.toString()
-                    result = result.slice(0, -1)
-                    resultContainer.textContent  = result
-                    console.log(typeof(borrar), borrar);
-                }
+                result = result.slice(0, -1)
+                resultContainer.textContent  = result
             } 
              else{
                 result = result + valor;
@@ -38,17 +31,51 @@ function returnValue(event){
             }
         }
     }
+
+    /*
+    for(i = 0; i < result.length; i += 3){
+        let resultComa = result.substring(i, i + 3)
+        resultComa = resultComa + ","
+        
+        console.log(result);
+    }*/
+    console.log(typeof(result));
+
 }
 
 function showResult(operacion){
     try{
-        const resultado = eval(operacion)
-        resultContainer.textContent = resultado
-        result = resultado
-        return result.toString()
+        resultContainer.textContent = eval(operacion)
+        result = resultContainer.textContent
+        result = result.toString()
+        return result
     } catch (error){
         resultContainer.textContent = "Error"
         result = " "
-        return "Error en la operación", result.toString()
+        return "Error en la operación", result
     }
 }
+
+let main = document.getElementById('main')
+let selectColor = document.getElementById('select-color')
+
+selectColor.addEventListener("click", themeSelector)
+
+function themeSelector() {
+    switch (selectColor.value) {
+        case "1":
+            main.classList.remove("theme2-main")
+            main.classList.remove("theme3-main")
+            break;
+        case "2":
+            main.classList.remove("theme3-main")
+            main.classList.add("theme2-main")
+            break;
+        case "3":
+            main.classList.remove("theme2-main")
+            main.classList.add("theme3-main")
+        default:
+            break;
+    }
+}
+
