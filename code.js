28e5e824d23f621ced614f3977949438e3349835
Frederick Morales pc1 <tocomorales30@gmail.com>
2023-09-22 13:10:04 -0600

    Arreglando bugs parte2

--- code.js
+++ code.js
@@ -4,6 +4,7 @@ let numsContainer = document.getElementById('numsContainer')
 /*result es un string*/ 
 let result = resultContainer.textContent
 let ultElemento
+let operacionHecha = false
 
 numsContainer.addEventListener("click", returnValue)
 
@@ -22,34 +23,40 @@ function returnValue(event){
         if(valor === "RESET"){
             result = " "
             resultContainer.textContent = result
+            operacionHecha = false
         }   else if(valor === "DEL"){
                 result = result.slice(0, -1)
                 resultContainer.textContent  = result
+                operacionHecha = false
             }   else if (valor === "x" && ultElemento !== "*" && ultElemento !== "+" && ultElemento !== "-" && ultElemento !== "/" && ultElemento !== "."){
-                    
                     result = result + "*"
-                    resultContainer.textContent = result
+                    resultContainer.textContent = result 
+                    operacionHecha = false
                 }   else if(valor === "="){
                         showResult(result)
+                        operacionHecha = true
                     }
         if (valor >= 0 && valor <= 10) {
-            result = result + valor
-            resultContainer.textContent = result
+                if (operacionHecha) {
+                    result = " "
+                    operacionHecha = false
+                }
+                result = result + valor
+                resultContainer.textContent = result
         }   else if (valor !== "x" && valor !== "RESET" && valor !== "DEL" && valor !== "=" && ultElemento !== "+" && ultElemento !== "-" && ultElemento !== "*" && ultElemento !== "/" && ultElemento !== ".") {
                 result = result + valor
                 resultContainer.textContent = result
+                operacionHecha = false
             }
-        
-
     }
     retornarUltimoElemento(result)
+    console.log(operacionHecha,typeof(operacionHecha));
 }
 function showResult(operacion){
     try{
         resultContainer.textContent = eval(operacion)
         result = resultContainer.textContent
-        /*result = Number(result)*/
-        return console.log(typeof(result));
+        operacionHecha = true
     } catch (error){
         resultContainer.textContent = "Error"
         result = " "
@@ -57,9 +64,7 @@ function showResult(operacion){
 }
 function retornarUltimoElemento(resultado){
     ultElemento = resultado[resultado.length - 1]
-    console.log(ultElemento);
 }
-
 
 /*SELECCIONAR COLOR*/
 let main = document.getElementById('main')
