5abd4d0fedf621ed726bd9b78662eb5ef2683d6a
Frederick Morales pc1 <tocomorales30@gmail.com>
2023-09-29 11:58:11 -0600

    Agregando comas al resultado final de la operacion

--- code.js
+++ code.js
@@ -4,7 +4,7 @@ let numsContainer = document.getElementById('numsContainer')
 /*result es un string*/ 
 let result = resultContainer.textContent
 let ultElemento
-let operacionHecha = false
+let operacionHecha = true
 
 numsContainer.addEventListener("click", returnValue)
 
@@ -33,12 +33,12 @@ function returnValue(event){
         }   
         else if (valor === "x" && ultElemento != " "){
             result = result + " * "
-            resultContainer.textContent = result 
+            resultContainer.textContent = result
             operacionHecha = false
         }   
         else if(valor === "="){
             showResult(result)
-            operacionHecha = true                    
+            operacionHecha = true
         }
         if (valor >= 0 && valor <= 10) {
             if (operacionHecha) {
@@ -61,21 +61,97 @@ function returnValue(event){
             
         }
         retornarUltimoElemento(result)
-        colocarComaAlResult(result)
     }
 }
+
 function showResult(operacion){
     try{
         resultContainer.textContent = eval(operacion)
         result = resultContainer.textContent
         operacionHecha = true
+        mostrarConComas(result)
+
     } catch (error){
         resultContainer.textContent = "Error"
         result = " "
     }
 }
+
 function retornarUltimoElemento(resultado){
     ultElemento = resultado[resultado.length - 1]
+}
+
+function colocarComa(cadena) {
+    let cadenaConComa = ""
+    if (cadena.length > 3) {
+        for (let i = cadena.length - 1, contador = 0; i >= 0; i--) {
+        cadenaConComa = cadena[i] + cadenaConComa
+        contador++
+        // Insertar una coma después de cada grupo de tres dígitos
+            if (contador === 3 && i !== 0) {
+                cadenaConComa = "," + cadenaConComa
+                contador = 0
+            }
+        }
+    } else{
+        cadenaConComa = cadena
+    }
+
+    return cadenaConComa
+}
+
+function colocarComaAlString(str) {
+    //let str = "9029889 + 2788.5998 * 2 + 323332 - 5848"
+    let cadenas
+    let strFinal = ""
+    
+
+        cadenas = str.split(" ") // CONVERTIMOS NUESTRO STRING EN UN ARRAY DE STRING POR CADA SPACIO ECONTRADO
+        //console.log(cadenas);
+        
+        for (let i = 0; i < cadenas.length; i++) {
+            let cadena = cadenas[i]
+            
+            //SI EL STRING CONTIENE UNA EXPRESION ARITMETICA SOLO LA SUMAMOS AL STRING FINAL
+            if (cadena === "+" || cadena === "-" || cadena === "*" || cadena === "/") {
+                strFinal = strFinal + " " + cadena + " "
+            } 
+            else if (cadena.includes(".")) {
+                let cadenaConPunto = cadena.split(".")
+                let cadenaConPuntoYComa = colocarComa(cadenaConPunto[0]) + "." + cadenaConPunto[1]
+                strFinal = strFinal + cadenaConPuntoYComa
+            } 
+            else if (cadena.length > 3) { // SI EL STRING ES MAYOR A 3 LE AGREGAMOS UNA COMA AL INICIO 
+                let cadenaConComa = colocarComa(cadena)
+                strFinal = strFinal + cadenaConComa
+            } 
+            else{
+                strFinal = strFinal + cadena
+            }
+            
+        }   
+
+return strFinal
+}
+
+function mostrarConComas(valor) {
+    
+    let valorConComa
+    
+    if (valor.includes(" ")) {
+        valorConComa = colocarComaAlString(valor)
+    }
+    else if (valor.includes(".")) {
+        let cadenaConPunto = valor.split(".")
+        let cadenaConPuntoYComa = colocarComa(cadenaConPunto[0]) + "." + cadenaConPunto[1]
+        valorConComa = cadenaConPuntoYComa
+    } else{
+        valorConComa = colocarComa(valor)
+    }
+
+    result = valorConComa
+    resultContainer.textContent = result
+    return result
 }
 
 /*SELECCIONAR COLOR*/
@@ -120,85 +196,3 @@ function themeSelector() {
             break;
     }
 }
-
-function colocarComa(cadena) {
-    let cadenaConComa = ""
-    if (cadena.length > 3) {
-        for (let i = cadena.length - 1, contador = 0; i >= 0; i--) {
-        cadenaConComa = cadena[i] + cadenaConComa
-        contador++
-        // Insertar una coma después de cada grupo de tres dígitos
-            if (contador === 3 && i !== 0) {
-                cadenaConComa = "," + cadenaConComa
-                contador = 0
-            }
-        }
-    }
-    return cadenaConComa
-}
-
-function colocarComaAlResult(str) {
-
-    let strFinal
-
-    // SI RESULT(STR) SOLO TIENE NUMEROS
-    if (str.length <= 3) {
-        strFinal = str
-        resultContainer.textContent = strFinal
-    }
-    else{
-        strFinal = colocarComa(str)
-        resultContainer.textContent = strFinal
-    }
-    console.log(strFinal);
-}
-
-
-/*
-function colocarComaAlResult(str) {
-    //let str = "9029889 + 2788.5998 * 2 + 323332 - 5848"
-    //let cadenas
-    let strFinal
-
-    if (str.includes(" ")) {
-
-        cadenas = str.split(" ") // CONVERTIMOS NUESTRO STRING EN UN ARRAY DE STRING POR CADA SPACIO ECONTRADO
-        //console.log(cadenas);
-        
-        for (let i = 0; i < cadenas.length; i++) {
-            let cadena = cadenas[i]
-            
-            //SI EL STRING CONTIENE UNA EXPRESION ARITMETICA SOLO LA SUMAMOS AL STRING FINAL
-            if (cadena === "+" || cadena === "-" || cadena === "*" || cadena === "/") {
-                strFinal = strFinal + " " + cadena + " "
-            } 
-            else{
-                
-                if (cadena.includes(".")) {
-                    let cadenaConPunoYComa
-                    cadenaConPunto = cadena.split(".")
-                    cadenaConPunoYComa = colocarComa(cadenaConPunto[0]) + "." + cadenaConPunto[1]
-                    strFinal = strFinal + cadenaConPunoYComa
-                } 
-                else if (cadena.length > 3) { // SI EL STRING ES MAYOR A 3 LE AGREGAMOS UNA COMA AL INICIO 
-                    let cadenaConComa = colocarComa(cadena)
-                    strFinal = strFinal + cadenaConComa
-                } 
-                else{
-                    strFinal = strFinal + cadena
-                }
-            }
-        }   
-    } /*else{
-        cadenas = str
-        strFinal = colocarComa(str)
-    }
-
-    if (str.length > 0) {
-        strFinal = colocarComa(str)
-    }
-    return strFinal
-}
-*/
-/*let prueba = "9029889 + 8 * 478748 + 8888 - 4888"
-console.log(colocarComaAlResult(prueba));*/
